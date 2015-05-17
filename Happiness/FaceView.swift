1a85fba51f2d2fbadb5912bd7ca10e368976a3ae
Mads Bielefeldt <mads.bielefeldt@gmail.com>
2015-05-17 13:57:46 +0200

    Eyes added

--- Happiness/FaceView.swift
+++ Happiness/FaceView.swift
@@ -8,14 +8,57 @@
 
 import UIKit
 
-class FaceView: UIView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+class FaceView: UIView
+{
+    var lineWidth: CGFloat = 3 { didSet { setNeedsDisplay() } }
+    var color = UIColor.blueColor() { didSet { setNeedsDisplay() } }
+    var scale: CGFloat = 0.90 { didSet { setNeedsDisplay() } }
+    
+    var faceCenter: CGPoint {
+        return convertPoint(center, fromView: superview)
     }
-    */
-
+    var faceRadius: CGFloat {
+        return min(bounds.size.height, bounds.size.width) / 2 * scale
+    }
+    
+    private struct Scaling {
+        static let FaceRadiusToEyeRadiusRatio: CGFloat = 10
+        static let FaceRadiusToEyeOffsetRatio: CGFloat = 3
+        static let FaceRadiusToEyeSeparationRatio: CGFloat = 1.5
+        static let FaceRadiusToMouthWidthRatio: CGFloat = 1
+        static let FaceRadiusToMouthHeigthRatio: CGFloat = 3
+        static let FaceRadiusToMouthOffsetRatio: CGFloat = 3
+    }
+    
+    private enum Eye { case Left, Right }
+    
+    private func bezierPathForEye(whichEye: Eye) -> UIBezierPath
+    {
+        let eyeRadius = faceRadius / Scaling.FaceRadiusToEyeRadiusRatio
+        let eyeVerticalOffset = faceRadius / Scaling.FaceRadiusToEyeOffsetRatio
+        let eyeHorizontalSeparation = faceRadius / Scaling.FaceRadiusToEyeSeparationRatio
+        
+        var eyeCenter = faceCenter
+        eyeCenter.y -= eyeVerticalOffset
+        
+        switch whichEye {
+            case .Left: eyeCenter.x -= eyeHorizontalSeparation / 2
+            case .Right: eyeCenter.x += eyeHorizontalSeparation / 2
+        }
+        
+        let eyePath = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        eyePath.lineWidth = lineWidth
+        return eyePath
+    }
+    
+    override func drawRect(rect: CGRect)
+    {
+        let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        facePath.lineWidth = lineWidth
+        color.set()
+        facePath.stroke()
+        
+        bezierPathForEye(.Left).stroke()
+        bezierPathForEye(.Right).stroke()
+    }
 }
