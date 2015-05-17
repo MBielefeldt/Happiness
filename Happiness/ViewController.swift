dbb1e52488fea596cbb7a769efd6a49a8e467aac
Mads Bielefeldt <mads.bielefeldt@gmail.com>
2015-05-17 12:06:57 +0200

    Initial

---
+++ Happiness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happiness
+//
+//  Created by Mads Bielefeldt on 17/05/15.
+//  Copyright (c) 2015 GN ReSound. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
