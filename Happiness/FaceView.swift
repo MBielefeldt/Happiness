7cda08f756ce17ac03600544909178fb7b126522
Mads Bielefeldt <mads.bielefeldt@gmail.com>
2015-05-17 12:17:37 +0200

    View controller and view added

---
+++ Happiness/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  Happiness
+//
+//  Created by Mads Bielefeldt on 17/05/15.
+//  Copyright (c) 2015 GN ReSound. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Mads Bielefeldt on 17/05/15.
+//  Copyright (c) 2015 GN ReSound. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController
+{
+
+}
