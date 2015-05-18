bb4533551010158926cc40b94d188e46577b6429
Mads Bielefeldt <mads.bielefeldt@gmail.com>
2015-05-18 08:09:40 +0200

    Gesture recognizers added

--- Happiness/HappinessViewController.swift
+++ Happiness/HappinessViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class HappinessViewController: UIViewController, FaceViewDataSource
 {
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 4.0
+    }
+    
     @IBOutlet weak var faceView: FaceView! {
         didSet {
             faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+        }
+    }
+    
+    @IBAction func pan(gesture: UIPanGestureRecognizer)
+    {
+        switch gesture.state {
+            case .Ended: fallthrough
+            case .Changed:
+                let translation = gesture.translationInView(faceView)
+                let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+                if happinessChange != 0 {
+                    happiness += happinessChange
+                    gesture.setTranslation(CGPointZero, inView: faceView)
+                }
+            default: break
         }
     }
     
